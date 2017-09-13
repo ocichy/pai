ea28870abd75d0599ca87af6e550309f0479b5c0
ocichy <furude6.rika@gmail.com>
2017-09-13 10:32:14 +0200

    funkcje

---
+++ js/funkcje.js
@@ -0,0 +1,13 @@
+function witaj(imie){
+    document.write('<br>Witaj '+ imie);
+}
+var name="Janusz";
+witaj(name);
+function poleProstokata(s, d) {
+    var s=document.getElementById('szerokosc').value;
+var d=document.getElementById('dlugosc').value;
+    w.innerHTML=s*d;
+    return pole;
+}
+var p=document.getElementById('przycisk');
+var w=document.getElementById('wyswietl');
